19b8d921ad7f4dda408556c0d62e26d6c31a6a0d
A01641937-alf <A01641937@itesm.mx>
2022-10-06 13:19:20 +0000

    Practica 2

---
+++ signal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int i = 1;
+
+int hola(int signalNumb){
+    printf("Recibi la senial %d ", signalNumb);
+}
+
+void terminarwhile(int signalNumber){
+    printf("Terminando while");
+    i = 0;
+}
+
+int main(){
+    signal(12,terminarwhile);
+    signal(2,hola);
+    while(1 == i){
+        printf("trabajando\n");
+        sleep(1);
+    }
+    printf("Aqui nunca llega\n");
+    return 0;
+}
