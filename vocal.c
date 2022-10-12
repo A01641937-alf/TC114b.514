6cfff502109fbda283e2d091090c198b33cc7416
A01641937-alf <A01641937@itesm.mx>
2022-10-12 12:54:02 +0000

    vocal

---
+++ vocal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+int main(){
+    char c;
+    char *msg = "1\n";
+    char *msg1 = "No es una vocal\n";
+    char may;
+    int n;
+    do{
+        n = read(STDIN_FILENO,&c,1);
+        may = toupper(c);
+        if(may == 'A' || may == 'E' || may == 'I' || may == 'O' || may == 'U' || may == 'a' || may == 'e' || may == 'i' || may == 'o' || may == 'u'){
+            write(STDOUT_FILENO,msg,14);
+        }
+    }while(n != 0);
+
+    return 0;
+}
