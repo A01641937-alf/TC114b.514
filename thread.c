8f45bcf8e2d9f26344781fb3111846b51a8a7c8a
A01641937-alf <A01641937@itesm.mx>
2022-10-20 13:27:10 +0000

    Practica 5

--- thread.c
+++ thread.c
@@ -1,26 +1,35 @@
 #include <pthread.h>
 #include <stdio.h>
 
+#define NUM_THREADS 10
+
+
 typedef struct thread_data{
     int threadId;
     char name[20];
-    //..
 } ThreadData;
 typedef char caracter;
 
-void *holaMundo(void *arg){
+void * helloWorld(void *arg){
     ThreadData *myData = (ThreadData *)arg;
-    printf("Hola desde el hilo y mi id es %d\n",myData->threadId);
+    printf("Hola desde el hilo y mi id es % d\n", myData -> threadId);
     pthread_exit(NULL);
+   //return NULL;
 }
 
+
 int main(){
-    for (int i = 0; i < 10; i++){
+    ThreadData threadData[NUM_THREADS];
+    for(int i = 0; i < NUM_THREADS; i++){
         ThreadData myData;
         pthread_t threadId;
         myData.threadId = i;
-        pthread_create(&threadId, NULL, holaMundo, (void *) &myData);
+        pthread_create(&threadId,NULL,helloWorld,(void *) &myData);
     }
-    pthread_exit(NULL);
-    printf ("NUNCA LLEGA");
+    pthread_exit(NULL); 
+    printf("Nunca llega");
+    //return 0;
 }
+
+//gcc -o thread thread.c -lpthread
+//./thread
